6ae6205e473d637d8c80828c00fa02912e2cbb85
Luciana is YammYcoding@freenode:6667 <luphp@yahoo.com.br>
2022-03-24 03:13:30 -0300

    help my love

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
 import { GetStaticProps } from 'next';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
+import Link from 'next/link';
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +28,65 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <main className={commonStyles.container}>
+        <Header />
+        <div className={styles.posts}>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Titulo</strong>
+              <p>Comentario</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 Mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Davi SAS
+                </li>
+              </ul>
+            </a>
+          </Link>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Titulo</strong>
+              <p>Comentario</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 Mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Davi SAS
+                </li>
+              </ul>
+            </a>
+          </Link>
+          <button type="button">Carregar mais posts</button>
+        </div>
+      </main>
+    </>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+    }
+  );
 
-//   // TODO
-// };
+  console.log(postsResponse.results);
+
+  return {
+    props: {
+      postsResponse,
+    },
+  };
+};
